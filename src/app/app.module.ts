c962fc188420c024be747ca5c487dca4bc88c5e5
Mateusz <mateusz.skrzypczyk1@hotmail.com>
2020-09-23 22:01:04 +0200

    cover

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CoverComponent } from './cover/cover.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    CoverComponent
   ],
   imports: [
     BrowserModule
